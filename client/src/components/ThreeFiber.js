441ae6bebc1d39f74e7873c8108c65771c47e760
Andrew Njoo <adnjoo@gmail.com>
2021-09-16 12:43:46 -0700

    edit

--- client/src/components/ThreeFiber.js
+++ client/src/components/ThreeFiber.js
@@ -6,38 +6,49 @@ import {artImages} from '../sharedVariables'
 
 const photo = artImages[Math.floor(Math.random()*artImages.length)]
 console.log('photo', photo)
+const textures = []
 const texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/mona2.png')
-// const texture1 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/kandinsky.png')
-const texture1 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/spots.png')
-
+const texture1 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/kandinsky.png')
+const texture2 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/flowers.png')
+const t3 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/ape1.png')
+textures.push(texture,texture1,texture2,t3)
+// console.log(textures)
 
 export const ThreeFiber = () => {
   function Box(props) {
+    const [count, setCount] = useState(1)
+    const [boxtex, setboxtex] = useState(texture)
     const ref = useRef()
-    // Set up state for the hovered and active state
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
-    // Subscribe this component to the render-loop, rotate the mesh every frame
+
     useFrame((state, delta) => (ref.current.rotation.y += 0.005))
-    // useFrame((state, delta) => (ref.current.rotation.x += 0.005))
+    const myfunction = () =>{
+      if(count==textures.length){
+        setCount(0)
+        setboxtex(textures[count])
+        return
+      }
+      setboxtex(textures[count])
+      setCount(count+1)
+    }
     return (
       <mesh
       {...props}
       ref={ref}
       scale={active ? 6 : 4.5}
-      onClick={(event) => setActive(!active)}
+      onClick={
+        // (event) => setActive(!active) make bigger
+        myfunction
+      }
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial 
-      map={texture}
+      map={boxtex}
       attach="material"
        />
-      <meshStandardMaterial 
-      map={texture1}
-      attach="material"
-       />
-      
+    
     </mesh>
     );
   }
