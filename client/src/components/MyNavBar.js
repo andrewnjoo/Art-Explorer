d912e978b97f7f2261938fdb5e626bb626e47c05
Andrew Njoo <adnjoo@gmail.com>
2021-10-03 16:41:15 -0700

    edit navbar

--- client/src/components/MyNavBar.js
+++ client/src/components/MyNavBar.js
@@ -18,7 +18,7 @@ const MyNavBar = ({ setAuth, isAuth, userName, getUserName }) => {
     return (
       <NavDropdown title="Login/Register">
         <NavDropdown.Item href={"/login"}>Login</NavDropdown.Item>
-        <NavDropdown.Item href={"/register"}>Get Started</NavDropdown.Item>
+        <NavDropdown.Item href={"/register"}>Register</NavDropdown.Item>
       </NavDropdown>
     );
   };
